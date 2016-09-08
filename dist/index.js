033ce41674cced4abb68a96553093ff4b8040e03
Downgoat <blue1086@gmail.com>
2016-09-08 05:02:40 +0000

    Added parser engine (LL/Recursive Decent)

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,137 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:true});var _typeof=typeof Symbol==="function"&&typeof Symbol.iterator==="symbol"?function(obj){return typeof obj}:function(obj){return obj&&typeof Symbol==="function"&&obj.constructor===Symbol?"symbol":typeof obj};var _createClass=function(){function defineProperties(target,props){for(var i=0;i<props.length;i++){var descriptor=props[i];descriptor.enumerable=descriptor.enumerable||false;descriptor.configurable=true;if("value"in descriptor)descriptor.writable=true;Object.defineProperty(target,descriptor.key,descriptor)}}return function(Constructor,protoProps,staticProps){if(protoProps)defineProperties(Constructor.prototype,protoProps);if(staticProps)defineProperties(Constructor,staticProps);return Constructor}}();var _fs=require("fs");var _fs2=_interopRequireDefault(_fs);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj}}function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function")}}var cheddarfile=function(){function cheddarfile(){var path=arguments.length<=0||arguments[0]===undefined?"cheddarfile":arguments[0];_classCallCheck(this,cheddarfile);if(!(typeof path==="string")){throw new TypeError("Value of argument \"path\" violates contract.\n\nExpected:\nstring\n\nGot:\n"+_inspect(path))}this.path=path;this.file=""}_createClass(cheddarfile,[{key:"ready",value:function ready(callback){var _this=this;_fs2.default.readFile(this.path,"utf-8",function(error,data){return callback((_this.file=data)&&_this,error)})}}]);return cheddarfile}();exports.default=cheddarfile;function _inspect(input,depth){var maxDepth=4;var maxKeys=15;if(depth===undefined){depth=0}depth+=1;if(input===null){return"null"}else if(input===undefined){return"void"}else if(typeof input==="string"||typeof input==="number"||typeof input==="boolean"){return typeof input==="undefined"?"undefined":_typeof(input)}else if(Array.isArray(input)){if(input.length>0){var _ret=function(){if(depth>maxDepth)return{v:"[...]"};var first=_inspect(input[0],depth);if(input.every(function(item){return _inspect(item,depth)===first})){return{v:first.trim()+"[]"}}else{return{v:"["+input.slice(0,maxKeys).map(function(item){return _inspect(item,depth)}).join(", ")+(input.length>=maxKeys?", ...":"")+"]"}}}();if((typeof _ret==="undefined"?"undefined":_typeof(_ret))==="object")return _ret.v}else{return"Array"}}else{var keys=Object.keys(input);if(!keys.length){if(input.constructor&&input.constructor.name&&input.constructor.name!=="Object"){return input.constructor.name}else{return"Object"}}if(depth>maxDepth)return"{...}";var indent="  ".repeat(depth-1);var entries=keys.slice(0,maxKeys).map(function(key){return(/^([A-Z_$][A-Z0-9_$]*)$/i.test(key)?key:JSON.stringify(key))+": "+_inspect(input[key],depth)+";"}).join("\n  "+indent);if(keys.length>=maxKeys){entries+="\n  "+indent+"..."}if(input.constructor&&input.constructor.name&&input.constructor.name!=="Object"){return input.constructor.name+" {\n  "+indent+entries+"\n"+indent+"}"}else{return"{\n  "+indent+entries+"\n"+indent+"}"}}}module.exports=exports["default"];
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
+var _parser2 = require('./parser');
+
+var _parser3 = _interopRequireDefault(_parser2);
+
+var _error = require('./error');
+
+var _error2 = _interopRequireDefault(_error);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var cheddarfile = function () {
+    function cheddarfile() {
+        var path = arguments.length <= 0 || arguments[0] === undefined ? "cheddarfile" : arguments[0];
+
+        _classCallCheck(this, cheddarfile);
+
+        if (!(typeof path === 'string')) {
+            throw new TypeError('Value of argument "path" violates contract.\n\nExpected:\nstring\n\nGot:\n' + _inspect(path));
+        }
+
+        this.path = path;
+
+        this.file = "";
+    }
+
+    _createClass(cheddarfile, [{
+        key: 'ready',
+        value: function ready(callback) {
+            var _this = this;
+
+            _fs2.default.readFile(this.path, "utf-8", function (error, data) {
+                return callback((_this.file = data) && _this, error);
+            });
+        }
+    }, {
+        key: 'parser',
+        value: function parser() {
+            return new _parser3.default(this.file, 0);
+        }
+    }]);
+
+    return cheddarfile;
+}();
+
+exports.default = cheddarfile;
+
+function _inspect(input, depth) {
+    var maxDepth = 4;
+    var maxKeys = 15;
+
+    if (depth === undefined) {
+        depth = 0;
+    }
+
+    depth += 1;
+
+    if (input === null) {
+        return 'null';
+    } else if (input === undefined) {
+        return 'void';
+    } else if (typeof input === 'string' || typeof input === 'number' || typeof input === 'boolean') {
+        return typeof input === 'undefined' ? 'undefined' : _typeof(input);
+    } else if (Array.isArray(input)) {
+        if (input.length > 0) {
+            var _ret = function () {
+                if (depth > maxDepth) return {
+                        v: '[...]'
+                    };
+
+                var first = _inspect(input[0], depth);
+
+                if (input.every(function (item) {
+                    return _inspect(item, depth) === first;
+                })) {
+                    return {
+                        v: first.trim() + '[]'
+                    };
+                } else {
+                    return {
+                        v: '[' + input.slice(0, maxKeys).map(function (item) {
+                            return _inspect(item, depth);
+                        }).join(', ') + (input.length >= maxKeys ? ', ...' : '') + ']'
+                    };
+                }
+            }();
+
+            if ((typeof _ret === 'undefined' ? 'undefined' : _typeof(_ret)) === "object") return _ret.v;
+        } else {
+            return 'Array';
+        }
+    } else {
+        var keys = Object.keys(input);
+
+        if (!keys.length) {
+            if (input.constructor && input.constructor.name && input.constructor.name !== 'Object') {
+                return input.constructor.name;
+            } else {
+                return 'Object';
+            }
+        }
+
+        if (depth > maxDepth) return '{...}';
+        var indent = '  '.repeat(depth - 1);
+        var entries = keys.slice(0, maxKeys).map(function (key) {
+            return (/^([A-Z_$][A-Z0-9_$]*)$/i.test(key) ? key : JSON.stringify(key)) + ': ' + _inspect(input[key], depth) + ';';
+        }).join('\n  ' + indent);
+
+        if (keys.length >= maxKeys) {
+            entries += '\n  ' + indent + '...';
+        }
+
+        if (input.constructor && input.constructor.name && input.constructor.name !== 'Object') {
+            return input.constructor.name + ' {\n  ' + indent + entries + '\n' + indent + '}';
+        } else {
+            return '{\n  ' + indent + entries + '\n' + indent + '}';
+        }
+    }
+}
+
+module.exports = exports['default'];
+//# sourceMappingURL=index.js.map
